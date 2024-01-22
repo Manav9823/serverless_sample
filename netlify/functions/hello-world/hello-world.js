ffe81974149a8889275d65bfdb158b4e8c6b038e
Manav9823 <manavbansalfddk@gmail.com>
2024-01-22 16:34:18 +0530

    more changes in code

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,11 +1,9 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const axios = require('axios')
 const brevoClientKey = 'hrg47x0s7k9e0le4uxx4m0si'
 
 const handler = async (event) => {
     console.log('hii')
-    // console.log(event)
-    const subject = event.queryStringParameters.name || 'World'
+    // const subject = event.queryStringParameters.name || 'World'
     const bodyRequest = JSON.parse(event.body)
     const email = bodyRequest.email
     const FIRSTNAME = bodyRequest.FIRSTNAME
@@ -24,16 +22,18 @@ const handler = async (event) => {
             email
         }
     };
-
-    return axios.request(options).then(function (response) {
-          console.log('response from brevo manav', response);
-          return {
-            statusCode: 200,
-            body: JSON.stringify({ message: `Hello ${subject}` }),
-          }
-        }).catch(function (error) {
-            console.error('error from brevo', error);
-        });
+    try{
+      const response = await axios(options)
+      return{
+        statusCode: 200,
+        body: JSON.stringify(response.data) 
+      }
+    } catch(error) {
+      return {
+        statusCode: 422,
+        body: `Error: ${error}`
+      }
+    }
   }
 
 module.exports = { handler }
